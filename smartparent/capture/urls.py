2565c45fb146486f0977f6106e71c0ab867ba616
Chencha Jacob <jmokua@gmail.com>
2024-07-26 11:09:30 +0100

    Able to process pdf and textual input

--- smartparent/capture/urls.py
+++ smartparent/capture/urls.py
@@ -6,4 +6,5 @@ app_name = 'capture'
 urlpatterns = [
     path("", views.index, name="index"),
     path("process_text_info/", views.process_text_info, name="process_text_info"),
+    path("process_pdf_upload/", views.process_pdf_upload, name="process_pdf_upload"),
 ]
--- smartparent/capture/views.py
+++ smartparent/capture/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from hashlib import md5
 from capture.commands.capture_info import CaptureInfo
+from capture.commands.capture_info_from_pdf import CaptureInfoFromPdf
 from capture.data_types.uploaded import UploadedContent
 import asyncio
+import tempfile
 
 def index(request):
     return render(request, 'capture/index.html')
@@ -16,5 +18,20 @@ def process_text_info(request):
         content=text)
         )
     asyncio.run(capture_info.execute())
-    return HttpResponse(capture_info.parsed_events)
-# Create your views here.
+    events = capture_info.parsed_events
+    return render(request, 'capture/event_details.html', {'events': events})
+
+def process_pdf_upload(request):
+    if request.method == 'POST' and request.FILES.get('pdf_file'):
+        pdf_file = request.FILES['pdf_file']
+        
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            for chunk in pdf_file.chunks():
+                temp_file.write(chunk)
+            temp_file_path = temp_file.name
+
+        capture_info = CaptureInfoFromPdf(file_path=temp_file_path)
+        asyncio.run(capture_info.execute())
+        return render(request, 'capture/event_details.html', {'events': capture_info.parsed_events})
+    else:
+        return HttpResponse("No file uploaded")
