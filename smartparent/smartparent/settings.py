14ac7846486405e3a9af1c74dfdb5024b6db168b
Chencha Jacob <jmokua@gmail.com>
2024-07-26 10:47:29 +0100

    Introduced base template

--- smartparent/smartparent/settings.py
+++ smartparent/smartparent/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'smartparent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'capture/templates/capture'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
