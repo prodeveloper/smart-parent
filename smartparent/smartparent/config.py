b47d57436c2dea321508a80f36bd43710ce66f32
Chencha Jacob <jmokua@gmail.com>
2024-07-28 18:27:26 +0100

    Linter changes for readability

--- smartparent/smartparent/config.py
+++ smartparent/smartparent/config.py
@@ -4,39 +4,78 @@ from collections import namedtuple
 import json
 
 class ConfigLoader:
+    """Loads and manages configuration settings for the application."""
+    database_config = namedtuple('DatabaseConfig', [
+            'PUBLIC_IP_ADDRESS', 'DATABASE_NAME', 'USER', 'PASSWORD'
+        ])
+    gemini_config = namedtuple('Config', [
+            'GEMINI_API_KEY', 'FIREBASE_SERVICE_ACCOUNT', 'LOCAL_PASSWORD', 'GOOGLE_APPLICATION_CREDENTIALS'
+        ])
+
+    def __init__(self):
+        self._db_configs()
+
     @property
     def configs(self):
+        """Retrieve Gemini configuration, prioritizing environment variables."""
         config = configparser.ConfigParser()
-        config.read('../config.ini') 
-        Config = namedtuple('Config', ['GEMINI_API_KEY', 'FIREBASE_SERVICE_ACCOUNT', 'LOCAL_PASSWORD', 'GOOGLE_APPLICATION_CREDENTIALS'])
+        config.read('../config.ini')
         # Prioritize environment variables set in Cloud Run
-        GEMINI_API_KEY = os.environ.get('gemini') if os.environ.get('gemini') else config.get('GEMINI', 'GEMINI_API_KEY', fallback=None)
-        LOCAL_PASSWORD = os.environ.get('password') if os.environ.get('password') else config.get('LOCAL', 'PASSWORD', fallback=None)
+        self.gemini_config.GEMINI_API_KEY = (
+            os.environ.get('gemini') or
+            config.get('GEMINI', 'GEMINI_API_KEY', fallback=None)
+        )
+        self.gemini_config.LOCAL_PASSWORD = (
+            os.environ.get('password') or
+            config.get('LOCAL', 'PASSWORD', fallback=None)
+        )
         #Because Firebase expects a dictionary while Google application credentials expects a string
-        if(os.environ.get('firebase')):
-            FIREBASE_SERVICE_ACCOUNT = json.loads(os.environ.get('firebase'))
-            GOOGLE_APPLICATION_CREDENTIALS = None
+        if os.environ.get('firebase'):
+            self.gemini_config.FIREBASE_SERVICE_ACCOUNT = json.loads(os.environ.get('firebase'))
+            self.gemini_config.GOOGLE_APPLICATION_CREDENTIALS = None
         else:
-            FIREBASE_SERVICE_ACCOUNT = config.get('GEMINI', 'FIREBASE_SERVICE_ACCOUNT', fallback=None)
-            GOOGLE_APPLICATION_CREDENTIALS = FIREBASE_SERVICE_ACCOUNT
+            self.gemini_config.FIREBASE_SERVICE_ACCOUNT = (
+                os.environ.get('firebase') or
+                config.get('GEMINI', 'FIREBASE_SERVICE_ACCOUNT', fallback=None)
+            )
+            self.gemini_config.GOOGLE_APPLICATION_CREDENTIALS = self.gemini_config.FIREBASE_SERVICE_ACCOUNT
 
 
-        return Config(GEMINI_API_KEY, FIREBASE_SERVICE_ACCOUNT, LOCAL_PASSWORD, GOOGLE_APPLICATION_CREDENTIALS)
-    
-    def get_prompt(self):
-        default_prompt = "Explain this to me maximum 5 bullet points as simply as possible. Use metaphors and analogies to explain the concepts."
-        prompt = os.environ.get('prompt') if os.environ.get('prompt') else default_prompt
-        return prompt
-    def max_tokens(self):
-        default_max_tokens = 1000
-        max_tokens = os.environ.get('max_tokens') if os.environ.get('max_tokens') else default_max_tokens
-        return max_tokens
-    
+        return self.gemini_config
+    def _db_configs(self):
+        config = configparser.ConfigParser()
+        config.read('../config.ini')
+        self.database_config = namedtuple('DatabaseConfig', [
+            'PUBLIC_IP_ADDRESS', 'DATABASE_NAME', 'USER', 'PASSWORD'
+        ])
+        self.database_config.PUBLIC_IP_ADDRESS = (
+            os.environ.get('PUBLIC_IP_ADDRESS') or
+            config.get('DATABASE', 'PUBLIC_IP_ADDRESS', fallback=None)
+        )
+        self.database_config.DATABASE_NAME = (
+            os.environ.get('DATABASE_NAME') or
+            config.get('DATABASE', 'DATABASE_NAME', fallback=None)
+        )
+        self.database_config.USER = (
+            os.environ.get('USER') or
+            config.get('DATABASE', 'USER', fallback=None)
+        )
+        self.database_config.PASSWORD = (
+            os.environ.get('PASSWORD') or
+            config.get('DATABASE', 'PASSWORD', fallback=None)
+        )
+
     def max_text_length(self):
         default_max_text_length = 10000
-        max_text_length = os.environ.get('max_text_length') if os.environ.get('max_text_length') else default_max_text_length
+        max_text_length = (
+            os.environ.get('max_text_length') or
+            default_max_text_length
+        )
         return max_text_length
     def max_times_run_today(self):
         default_max_times_run_today = 20
-        max_times_run_today = os.environ.get('max_times_run_today') if os.environ.get('max_times_run_today') else default_max_times_run_today
+        max_times_run_today = (
+            os.environ.get('max_times_run_today') or
+            default_max_times_run_today
+        )
         return max_times_run_today
